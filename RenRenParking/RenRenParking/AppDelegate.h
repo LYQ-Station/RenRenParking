bd575614f98911ef315d31c885dc42fe2f8c5f17
LiYongQiang <coderfly@163.com>
2014-10-22 21:47:23 +0800

    init project

---
+++ RenRenParking/RenRenParking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RenRenParking
+//
+//  Created by LiYongQiang on 14/10/22.
+//  Copyright (c) 2014年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RenRenParking/RenRenParking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RenRenParking
+//
+//  Created by LiYongQiang on 14/10/22.
+//  Copyright (c) 2014年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
